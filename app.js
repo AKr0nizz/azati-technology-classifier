e62eebb6e2d80e09cc450e4878a1c58dc237b061
Konstantin Lebejko <konstantin.lebejko@gmail.com>
2020-12-09 20:28:36 +0300

    feat: updated technologies list

--- app.js
+++ app.js
@@ -1,46 +1,52 @@
-let io = require('./modules/io.js');
+let io = require("./modules/io.js");
 
 settings = {
-    inputFilePath: './data/input.txt',
-    outputFilePath: './data/output.json',
-    technologiesFilePath: './models/technologies.json',
-}
+  inputFilePath: "./data/input.txt",
+  outputFilePath: "./data/output.json",
+  technologiesFilePath: "./models/technologies.json",
+};
 
 runScript = async () => {
-    // Extract input data and process it to the array
-    let inputData = await io.readFile(settings.inputFilePath);
-    inputData = inputData.split('\n').map(x => x.trim());
+  // Extract input data and process it to the array
+  let inputData = await io.readFile(settings.inputFilePath);
+  inputData = inputData.split("\n").map((x) => x.trim());
 
-    // Extract technologies classification model from JSON
-    const inputTechnologies = JSON.parse(await io.readFile(settings.technologiesFilePath))
+  // Extract technologies classification model from JSON
+  const inputTechnologies = JSON.parse(
+    await io.readFile(settings.technologiesFilePath)
+  );
 
-    // Create duplicate object and will it with dummy data
-    let sample_object = {
-        "technologies": {}
+  // Create duplicate object and will it with dummy data
+  let sample_object = {
+    technologies: {},
+  };
+  for (const key in inputTechnologies["technologies"]) {
+    sample_object["technologies"][key] = [];
+  }
+
+  console.log(inputData.length);
+
+  // Process the data
+  inputData.forEach((inputElement) => {
+    for (const technologyType in inputTechnologies["technologies"]) {
+      if (
+        inputTechnologies["technologies"][technologyType]
+          .map((x) => x.toLowerCase())
+          .includes(inputElement.toLowerCase())
+      ) {
+        sample_object["technologies"][technologyType].push(inputElement);
+        inputData = inputData.filter((item) => item !== inputElement);
+        break;
+      }
     }
-    for (const key in inputTechnologies["technologies"]) {
-        sample_object["technologies"][key] = []
-    }
+  });
 
-    console.log(inputData.length)
+  console.log(inputData.length);
 
-    // Process the data
-    inputData.forEach(inputElement => {
-        for (const technologyType in inputTechnologies["technologies"]) {
-            if (inputTechnologies["technologies"][technologyType].map(x => x.toLowerCase()).includes(inputElement.toLowerCase())) {
-                sample_object["technologies"][technologyType].push(inputElement)
-                inputData = inputData.filter(item => item !== inputElement)
-                break;
-            }
-        }
-    });
+  // Add data as unclassifies
+  sample_object["technologies"]["Unclassified"] = inputData;
 
-    console.log(inputData.length)
-
-    // Add data as unclassifies
-    sample_object["technologies"]["Unclassified"] = inputData
-
-    console.log(sample_object["technologies"]["Unclassified"]);
-}
+  console.log(sample_object["technologies"]["Unclassified"]);
+};
 
 runScript();
--- assets/js/VueInit.js
+++ assets/js/VueInit.js
@@ -1,44 +1,73 @@
 var app = new Vue({
-    el: '#vue-app',
-    data: {
-        inputData: '',
-        sampleTechnologies: {},
-        sortedTechnologies: {},
-        technologyStackLocation: './assets/data/technologies.json'
+  el: "#vue-app",
+  data: {
+    inputData: "",
+    sampleTechnologies: {},
+    sortedTechnologies: {},
+    technologyStackLocation: "./assets/data/technologies.json",
+  },
+  created() {
+    this.loadTechnologies();
+  },
+  methods: {
+    loadTechnologies() {
+      fetch(this.technologyStackLocation)
+        .then((response) => response.json())
+        .then((response) => {
+          this.sampleTechnologies = response.technologies;
+        });
     },
-    created() {
-        this.loadTechnologies()
-    },
-    methods: {
-        loadTechnologies() {
-            fetch(this.technologyStackLocation).then((response) => response.json()).then((response) => {
-                this.sampleTechnologies = response.technologies
-            })
-        },
-        handleClassificationButtonClick() {
-            let cleansedInputData = this.inputData.split('\n').map(x => x.trim());
+    handleClassificationButtonClick() {
+      let cleansedInputData = this.inputData.split("\n").map((x) => x.trim());
 
-            // Create and fill sample object
-            let sampleObject = {
-                "technologies": {}
-            };
-            for (const key in this.sampleTechnologies) {
-                sampleObject["technologies"][key] = [];
-            }
+      // Create and fill sample object
+      let sampleObject = {
+        technologies: {},
+      };
+      for (const key in this.sampleTechnologies) {
+        sampleObject["technologies"][key] = [];
+      }
 
-            // Process the data
-            cleansedInputData.forEach(inputElement => {
-                for (const technologyType in this.sampleTechnologies) {
-                    if (this.sampleTechnologies[technologyType].map(x => x.toLowerCase()).includes(inputElement.toLowerCase())) {
-                        sampleObject["technologies"][technologyType].push(inputElement);
-                        cleansedInputData = cleansedInputData.filter(item => item !== inputElement);
-                        break;
-                    }
-                }
-            });
-
-            sampleObject["technologies"]["Unclassified"] = cleansedInputData;
-            this.sortedTechnologies = sampleObject["technologies"];
+      // Process the data
+      cleansedInputData.forEach((inputElement) => {
+        for (const technologyType in this.sampleTechnologies) {
+          if (
+            this.sampleTechnologies[technologyType]
+              .map((x) => x.toLowerCase())
+              .includes(inputElement.toLowerCase())
+          ) {
+            sampleObject["technologies"][technologyType].push(inputElement);
+            cleansedInputData = cleansedInputData.filter(
+              (item) => item !== inputElement
+            );
+            break;
+          }
         }
-    }
-})
+      });
+
+      // Clean unclassified from headlines
+      // Get all technology types from JSON
+      const techHeadlines = Object.keys(this.sampleTechnologies);
+
+      // Filter data to remove headlines from unclassified (if some)
+      for (const key in techHeadlines) {
+        if (techHeadlines.hasOwnProperty(key)) {
+          const element = techHeadlines[key];
+          if (
+            cleansedInputData
+              .map((x) => x.toLowerCase())
+              .includes(element.toLowerCase())
+          ) {
+            cleansedInputData = cleansedInputData.filter(
+              (item) => item !== element
+            );
+          }
+        }
+      }
+
+      sampleObject["technologies"]["Unclassified"] = cleansedInputData;
+
+      this.sortedTechnologies = sampleObject["technologies"];
+    },
+  },
+});
